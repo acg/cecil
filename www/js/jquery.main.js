e318f34ebd615e2825f8f8d5cf75e96adb92525b
Alan Grow <alangrow@gmail.com>
2011-04-01 03:23:13 -0400

    add colorful progress bar

--- www/js/jquery.main.js
+++ www/js/jquery.main.js
@@ -1,6 +1,45 @@
 ;(function($) {
 
+
+$.fn.percentageBar = function() {
+  $(this).each( function(i,elem) {
+
+    var parts = $(elem).text().split(' ');
+    var x = parseFloat(parts[0]);
+    var y = parseFloat(parts[2]);
+
+    var percent = 0;
+    var color;
+
+    if (y > 0.0)
+      percent = Math.round( 100 * x / y );
+
+    if (percent <= 100)
+    {
+      var color1 = [ 0x00, 0x40, 0x80 ];
+      var color2 = [ 0x30, 0xb0, 0x40 ];
+      color = '';
+
+      for (i=0; i<3; i++) {
+        var c = Math.round( color1[i] + percent * (color2[i] - color1[i]) / 100 );
+        var hex = c.toString(16);
+        while (hex.length < 2) hex = "0"+hex;
+        color += hex;
+      }
+    }
+
+    var bar = $('<div class="percentage"><div class="text">'+percent+' %</div><div class="pos">&nbsp;</div></div>');
+    $('.pos', bar).css({ width: percent+'%', 'background-color': '#'+color });
+    if (percent > 100) $(bar).addClass("over");
+    if (y == 0) $(bar).addClass("unknown");
+    $(elem).prepend(bar);
+
+  } );
+};
+
+
 $(document).ready( function() {
+
   $('table').tablesorter();
 
   $('.filter').change( function() {
@@ -9,6 +48,8 @@ $(document).ready( function() {
     form.find('.filter:input[value=""]').attr('disabled',true);
     form.submit();
   } );
+
+  $('.field-Progress').percentageBar();
 
 } );
 
