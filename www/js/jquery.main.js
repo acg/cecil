d2f6a7cc0d14cbfb00929533077fcbbd9e5f4410
Alan Grow <alangrow@gmail.com>
2011-03-31 21:17:51 -0400

    de-clutter filter query strings

--- www/js/jquery.main.js
+++ www/js/jquery.main.js
@@ -2,7 +2,14 @@
 
 $(document).ready( function() {
   $('table').tablesorter();
-  $('.filter').change( function() { $('form').submit(); } ); /* TODO clear empty fields */
+
+  $('.filter').change( function() {
+    var form = $('form.mainform');
+    /* Remove empty fields to avoid cluttering the query string */
+    form.find('.filter:input[value=""]').attr('disabled',true);
+    form.submit();
+  } );
+
 } );
 
 })(jQuery);
